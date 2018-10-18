bbbc4ece0e299d283e1033048160687e701ac21a
Theodore Koutros <koutrost@student.ethz.ch>
2018-10-18 15:33:45 -0400

    Backup

--- Vision Testing/select_input.py
+++ Vision Testing/select_input.py
@@ -2,10 +2,19 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 
 
 t = np.arange(10)
 plt.plot(t, np.sin(t))
+
+img = cv2.imread('finger.jpg')
+
+shape = img.shape
+
+print shape
+
+
 print("Please click")
 x = plt.ginput(3)
 print("clicked", x)
