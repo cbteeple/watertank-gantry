cf081a6eb7fbb86c21dfe392839cc88785a5ba75
cbteeple <cbteeple@g.harvard.edu>
2018-11-10 21:03:44 -0500

    updated gitignore to actually sync CMAKE files...oops

--- ROS/catkin_ws/src/gantry_control/scripts/valves_gcode_control.py
+++ ROS/catkin_ws/src/gantry_control/scripts/valves_gcode_control.py
@@ -14,9 +14,16 @@ class valvesGcode(object):
         #Set up variables
         self.ready=False
 
+		params=rospy.get_param('gantry')
+		self.devname=params['devname']
+		self.baud=params['baudrate']
+
         #Set up publishers and subscribers
         self.sub_ready = rospy.Subscriber("/gantry/ready", Bool, self.callbackReady)
 		self.sub_set   = rospy.Subscriber("/gantry/set_actuation", actuation, self.callbackGo)
+
+		#Send a "start" message
+		rospy.loginfo("[%s] has started", self.node_name)
 
 
 	def callbackReady(self, data):
@@ -24,14 +31,14 @@ class valvesGcode(object):
 
 
 	def callbackGo(self, data):
-		go(data.levels)
+		self.go(data.levels)
 
 
 	def getArray(self, values):
 		#Set up GCODE based on values
 		gcode_array=[]
 		for idx, value in enumerate(values):
-			val_mapped=map28bit(value)
+			val_mapped=self.map28bit(value)
 			if val_mapped==0:
 				gcode_array.append('M107 P%d'%(idx))
 			else:
@@ -41,14 +48,9 @@ class valvesGcode(object):
 
 
 	def go(self, values):
-		gcode_array = getArray(values)
+		gcode_array = self.getArray(values)
 		
-		#Set up the serial connection
-		params=rospy.get_param('gantry')
-		devname=params['devname']
-		baud=params['baudrate']
-		
-		s = sendGcodeSerial.obtain(devname,baud)
+		s = sendGcodeSerial.obtain(self.devname,self.baud)
 		
 		#Send the GCODE commands
 		for entry in gcode_array:
@@ -69,7 +71,7 @@ if __name__ == '__main__':
     try:
     	rospy.init_node('valve_control', anonymous=True)
     	valve_control_node = valvesGcode()
-    	
+
         while not rospy.is_shutdown():
 			rospy.spin()  #keep python from exiting until this node is stopped
 
